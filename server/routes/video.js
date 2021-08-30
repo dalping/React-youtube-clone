5980e816040ccefeff3b9866262de51d1e6b6d39
Dalping <kbing14@naver.com>
2021-08-30 15:47:56 +0900

    ~dropzone 중간 저장

---
+++ server/routes/video.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const router = express.Router();
+// //const { Video } = require("../models/Video");
+// const multer = require("multer");
+// const { auth } = require("../middleware/auth");
+
+// //=================================
+// //             Video
+// //=================================
+
+
+// router.post('/uploadfiles',(req, res) =>{
+//     // 비디오를 서버에 저장한다.
+// })
+
+// module.exports = router;  
