9499f52c8ab975002beed622e9236a082902071e
Dalping <kbing14@naver.com>
2021-08-31 01:36:33 +0900

    ffmpeg

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,145 @@
+import React,{useState} from 'react'
+import {Typography, Button, Form, message, Input, Icon} from 'antd';
+import DropZone from 'react-dropzone';
+
+import axios from 'axios';
+
+const {Title} = Typography;
+const {TextArea} = Input;
+
+const PrivateOption = [
+    {value:0, label:"Private"},
+    {value:1, label:"Public"}
+]
+
+const CategoryOption = [
+    {value:0, label:"Film & Animation"},
+    {value:1, label:"Auto & Vehicles"},
+    {value:2, label:"Music"},
+    {value:3, label:"Pets & Animals"}
+]
+
+function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState('')
+    const [Description, setDescription] = useState('')
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+
+    function onTitleChange(e){
+        setVideoTitle(e.target.value);
+    }
+    
+    function onDescriptionChange(e){
+        setDescription(e.target.value);
+    }
+
+    function onPrivateChange(e){
+        setPrivate(e.currentTarget.value);
+    }
+
+    function onCategoryChange(e){
+        setCategory(e.currentTarget.value);
+    }
+
+    function onDrop(files){ 
+        let formData = new FormData;
+        const config={
+            header:{'content-type' : 'multipart/form-data'}
+        }
+            formData.append("file", files[0])
+            
+            axios.post('/api/video/uploadfiles', formData, config) //서버에 리퀘스트 보내기
+            .then(response => {
+                if(response.data.success){ //동영상 업로드 성공!
+                    // console.log(response.data)
+                    //썸네일 생성
+                    let variable = {
+                        url:response.data.url,
+                        fileName: response.data.fileName
+                    }
+
+                    axios.post('/api/video/thumbnail', variable) // 썸네일 리퀘스트 보내기
+                    .then(response => {
+                        if(response.data.success){
+                        }else{
+                            console.log(response);
+                            alert("썸네일 생성에 실패했습니다.")
+                        }
+                    })
+
+                }else{
+                    console.log(response);
+                    alert('비디오 업로드를 실패했습니다.')
+                }
+            }) 
+        }       
+   
+    return (
+        <div style={{maxWidth:'700px', margin:'2rem auto'}}>
+            <div style={{textAlign:'center', marginBottom:'2rem'}}>
+            <Title level={2}>Upload Video</Title>
+            </div>
+            <div>
+
+            <Form onSubmit>
+                    <div style={{display:'flex', justifyContent:'space-between'}}>
+                        <DropZone
+                        onDrop = {onDrop}
+                        multiple = {false}
+                        maxSize = {800000000}>
+                            {({getRootProps, getInputProps}) => (
+                            <div style={{ width: '300px', height:'240px', border:'1px solid lightgray',
+                            display:'flex',alignItems:'center', justifyContent:'center'}}{...getRootProps()}>
+                            <input {...getInputProps()}/>
+                            <Icon type='plus' style={{fontSize:'3rem'}} />
+                            </div>
+                        )}
+                        </DropZone>
+                    </div>
+                    <div>
+                        <img src alt/>
+                    </div>
+
+                    <br/>
+                    <br/>
+                    <label>Title</label>
+                    <Input 
+                        onChange={onTitleChange}
+                        value={VideoTitle}
+                    />
+                    <br/>
+                    <br/>
+                    <label>Description</label>
+                    <TextArea
+                        onChange={onDescriptionChange}
+                        value = {Description}
+                    />
+                    <br/>
+                    <br/>
+                    <select onChange={onPrivateChange}>
+                        {PrivateOption.map((item,index) =>(
+                            <option key={index} value={item.value}>{item.label}</option>
+                        ))}
+                    </select>
+                    <br/>
+                    <br/>
+                    <select onChange={onCategoryChange}>
+                        {CategoryOption.map((item,index) =>(
+                            <option key={index} value={item.value}>{item.label}</option>
+                        ))}
+                    </select>
+                    <br/>
+                    <br/>
+                    <Button type="primary" size="large" onClick>
+                        Submit
+                    </Button>
+
+                    
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+export default VideoUploadPage
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,16 +1,78 @@
-// const express = require('express');
-// const router = express.Router();
-// //const { Video } = require("../models/Video");
-// const multer = require("multer");
-// const { auth } = require("../middleware/auth");
+const express = require('express');
+const router = express.Router();
+const { auth } = require("../middleware/auth");
+const multer = require("multer");
 
-// //=================================
-// //             Video
-// //=================================
+var ffmpeg = require("fluent-ffmpeg");
 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.mp4') {
+            return cb(res.status(400).end('only jpg, png, mp4 is allowed'), false);
+        }
+        cb(null, true)
+    }
+})
 
-// router.post('/uploadfiles',(req, res) =>{
-//     // 비디오를 서버에 저장한다.
-// })
+var upload = multer({ storage: storage }).single("file")
 
-// module.exports = router;  
+//=================================
+//             Video
+//=================================
+
+router.post("/uploadfiles", (req, res) => {
+
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+    })
+
+});
+
+router.post("/thumbnail", (req, res) => {
+
+    let filePath ="";
+    let fileDuration ="";
+    //비디오 정보 가져오기
+    //ffmpeg.setFfmpegPath('C:\Users\kbk\Desktop\ffmpeg\ffmpeg\bin\ffmpeg.exe');
+    ffmpeg.ffprobe(req.body.url, function(err, metadata){
+        console.dir(metadata); //all metadata
+        console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration;
+    });
+
+    ffmpeg(req.body.url)
+        .on('filenames', function (filenames) { //썸네일의 파일네임 생성
+            console.log('Will generate ' + filenames.join(', '))
+            console.log(filenames)
+            
+            filePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () { //썸네일 생성 후 무엇을 할것인지?
+            console.log('Screenshots taken');
+            return res.json({ success: true, url: filePath, fileName:filenames, fileDuration: fileDuration})
+        })
+        .on('error',function(err){ //에러 발생 시
+            console.error(err);
+            return res.json({success: false, err});
+        })
+        .screenshots({ //
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3, //찍을 썸네일 갯수
+            folder: 'uploads/thumbnails', //썸네일 저장위치
+            size:'320x240', //썸네일 사이즈
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
+});
+
+module.exports = router;
